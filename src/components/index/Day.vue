2c8e73dd4beec6eb7fd8566365ba5247ab0b77ac
padosum <makalu171@gmail.com>
2022-01-22 23:56:09 +0900

    #1 Install VitePress

---
+++ src/components/index/Day.vue
@@ -0,0 +1,42 @@
+<template>
+    <template v-if="isComplete">
+        <a :href="date" class="day">
+            {{ date }}
+            <div class="label" :style="{ backgroundColor: color }">
+                {{ label }}
+            </div>
+        </a>
+    </template>
+    <template v-else>
+        <div :class="isPlaceholder ? `day` : `placeholder day`">
+            {{ date }}
+        </div>
+    </template>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+    date: Number,
+    complete: Boolean,
+    label: String,
+    color: String,
+    placeholder: Boolean,
+})
+
+const isComplete = computed({
+  get () {
+    return props.complete
+  },
+})
+
+const isPlaceholder = computed({
+    get () {
+        return props.placeholder
+    }
+})
+</script>
+<style scoped>
+
+</style>
